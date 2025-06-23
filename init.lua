702b4c7ace74e22bb939ecc05359db84a0360a77
sebaperz <juan.s.perez.inbox@gmail.com>
2025-06-23 18:43:50 -0500

    wip

--- init.lua
+++ init.lua
@@ -1,44 +1,17 @@
-require ("core.settings")
-require ("core.keymaps")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+-- [[ Setting options ]]
+require 'settings'
 
-require('lazy').setup {
-  'NMAC427/guess-indent.nvim',
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
-  require 'plugins.autocompletion',
-  require 'plugins.format',
-  require 'plugins.flash',
-  require 'plugins.gitsigns',
-  require 'plugins.lsp',
-  require 'plugins.mini',
-  require 'plugins.oil',
-  require 'plugins.telescope',
-  require 'plugins.terminal',
-  require 'plugins.theme',
-  require 'plugins.treesitter',
-  require 'plugins.whichkey',
-}
+-- [[ Basic Keymaps ]]
+require 'keymaps'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
